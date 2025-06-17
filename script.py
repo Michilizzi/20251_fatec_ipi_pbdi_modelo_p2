bb4b89e4e043ce81c37fa6b4e1d81cc37b22289c
danz0r408 <mussoidaniel@gmail.com>
2025-06-17 10:54:58 -0300

    fix: return #req2

--- script.py
+++ script.py
@@ -10,6 +10,7 @@ from sklearn.compose import ColumnTransformer
 #essa função deve devolver a base de dados
 def ler_base():
   dataset = pd.read_csv(r'dados.csv')
+  return dataset
 
   #REQ 3
 #essa função recebe a base lida anteriormente
